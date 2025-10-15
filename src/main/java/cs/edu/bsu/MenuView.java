b96b0fc8a00e3ae2e961d3b56d6fb41a8e734352
dillonj4343 <djacks3375@gmail.com>
2025-10-15 16:20:29 -0400

    Write MenuView for title

--- src/main/java/cs/edu/bsu/MenuView.java
+++ src/main/java/cs/edu/bsu/MenuView.java
@@ -1,4 +1,29 @@
 package cs.edu.bsu;
 
-public class MenuView extends {
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class MenuView extends BorderPane {
+    public MenuView() {
+        Label title = new Label("MAAD Casino");
+        setTop(title);
+        BorderPane.setAlignment(title, Pos.CENTER);
+
+        Button blackjackButton = new Button("Blackjack");
+        Button warButton = new Button("War");
+
+        VBox box = new VBox(10, blackjackButton, warButton);
+        box.setAlignment(Pos.CENTER);
+        setCenter(box);
+    }
+    private void showInfo(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
