33e759fda0008fe99f86ec1cf090196d14d38832
ananya12sharmaa <ananya2023sharma@gmail.com>
2025-10-22 17:55:39 -0400

    adding further classes for the 2 games

---
+++ src/main/java/cs/edu/bsu/BlackjackView.java
@@ -0,0 +1,22 @@
+package cs.edu.bsu;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class BlackjackView extends VBox {
+
+    public BlackjackView() {
+        Label title = new Label("♣ Blackjack ♠");
+        title.getStyleClass().add("title");
+
+        Button back = new Button("Return to Menu");
+        back.getStyleClass().add("red");
+        back.setOnAction(e -> getScene().setRoot(new MenuView()));
+
+        setAlignment(Pos.CENTER);
+        setSpacing(20);
+        getChildren().addAll(title, back);
+    }
+}
