e88fd3fdf3bfd5cee2c044e29899fb65d5384872
ananya12sharmaa <ananya2023sharma@gmail.com>
2025-10-22 17:51:03 -0400

    updating GUI

--- src/main/java/cs/edu/bsu/MenuView.java
+++ src/main/java/cs/edu/bsu/MenuView.java
@@ -1,45 +1,83 @@
 package cs.edu.bsu;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Alert;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 
 public class MenuView extends BorderPane {
+
     public MenuView() {
         Label title = new Label("MAAD Casino");
         title.getStyleClass().add("title");
-        setTop(title);
         BorderPane.setAlignment(title, Pos.CENTER);
 
-        Button blackjack = new Button("Blackjack");
-        blackjack.getStyleClass().add("black");
+        Label player = new Label("Player: Guest");
+        player.getStyleClass().add("stat");
 
-        Button war = new Button("War");
-        war.getStyleClass().add("red");
+        Label dot = new Label("•");
+        dot.getStyleClass().add("stat-dot");
 
-        Button horseracing = new Button("Horse Racing");
-        horseracing.getStyleClass().add("black");
+        Label coins = new Label("MAAD Coins: 1,000");
+        coins.getStyleClass().add("stat");
 
-        Button slots = new Button("Slots");
-        slots.getStyleClass().add("red");
+        HBox statusBar = new HBox(10, player, dot, coins);
+        statusBar.setAlignment(Pos.CENTER);
+        statusBar.setPadding(new Insets(5, 0, 20, 0));
 
-        Button roulette = new Button("Roulette");
-        roulette.getStyleClass().add("black");
+        VBox topBox = new VBox(title, statusBar);
+        topBox.setAlignment(Pos.CENTER);
+        setTop(topBox);
 
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(24);
+        grid.setVgap(24);
+        grid.setPadding(new Insets(20));
 
-        VBox box = new VBox(10, blackjack, war, horseracing, slots, roulette);
-        box.setAlignment(Pos.CENTER);
-        setCenter(box);
+        ColumnConstraints c = new ColumnConstraints();
+        c.setPercentWidth(33.333);
+        RowConstraints r = new RowConstraints();
+        r.setPercentHeight(33.333);
+        grid.getColumnConstraints().addAll(c, c, c);
+        grid.getRowConstraints().addAll(r, r, r);
+
+        Button blackjack = makeButton("Blackjack", "black");
+        blackjack.setOnAction(e -> getScene().setRoot(new BlackjackView()));
+
+        Button war = makeButton("War", "red");
+        war.setOnAction(e -> getScene().setRoot(new WarView()));
+
+        Button slots = makeButton("Slots", "green");
+        slots.setOnAction(e -> showInfo("Slots coming soon!"));
+
+        Button horse = makeButton("Horse Racing", "red");
+        horse.setOnAction(e -> showInfo("Horse Racing coming soon!"));
+
+        Button roulette = makeButton("Roulette", "black");
+        roulette.setOnAction(e -> showInfo("Roulette coming soon!"));
+
+        grid.add(blackjack, 0, 0);
+        grid.add(war, 2, 0);
+        grid.add(slots, 1, 1);
+        grid.add(horse, 0, 2);
+        grid.add(roulette, 2, 2);
+        setCenter(grid);
     }
-    private void showInfo(String message) {
+
+    private Button makeButton(String text, String cssClass) {
+        Button btn = new Button(text);
+        btn.getStyleClass().add(cssClass);
+        btn.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+        return btn;
+    }
+
+    private void showInfo(String msg) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setHeaderText(null);
-        alert.setContentText(message);
+        alert.setContentText(msg);
         alert.showAndWait();
     }
 }
-
-//TEST BY MATTEO
