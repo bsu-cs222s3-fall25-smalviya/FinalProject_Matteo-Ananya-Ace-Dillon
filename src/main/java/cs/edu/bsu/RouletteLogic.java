8665c47370bf885394ac4fd74d35af2df678847a
dillonj4343 <djacks3375@gmail.com>
2025-11-04 13:33:55 -0500

    Update Roulette Logic

--- src/main/java/cs/edu/bsu/RouletteLogic.java
+++ src/main/java/cs/edu/bsu/RouletteLogic.java
@@ -94,9 +94,9 @@ public class RouletteLogic {
             case BLACK -> BLACK_NUMBERS.contains(rolledNumber);
             case EVEN -> rolledNumber != 0 && rolledNumber % 2 == 0;
             case ODD -> rolledNumber % 2 == 1;
-            case DOZEN1 -> rolledNumber >= 1 && rolledNumber <= 12;
-            case DOZEN2 -> rolledNumber >= 13 && rolledNumber <= 24;
-            case DOZEN3 -> rolledNumber >= 25 && rolledNumber <= 36;
+//            case DOZEN1 -> rolledNumber >= 1 && rolledNumber <= 12;
+//            case DOZEN2 -> rolledNumber >= 13 && rolledNumber <= 24;
+//            case DOZEN3 -> rolledNumber >= 25 && rolledNumber <= 36;
         };
     }
 
